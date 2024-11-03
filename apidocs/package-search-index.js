c3a2c34c316d17967bf2b9dc0574299d8d543943
robtimus <robtimus@users.noreply.github.com>
2024-11-03 13:17:12 +0000

    Updated for version 1.0

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.robtimus.maven.plugins.resourcelist"}]
---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.github.robtimus.maven.plugins.resourcelist","l":"ListResourcesMojo"},{"p":"com.github.robtimus.maven.plugins.resourcelist","l":"ResourceListClass"}]
